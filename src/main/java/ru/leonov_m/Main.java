738bdcbd3de3de72d57c1fddcecac5c77d86e0a9
Jim-jimjim <69318828+Jim-jimjim@users.noreply.github.com>
2022-11-03 19:40:52 +0300

    Поиск строк

--- src/main/java/ru/leonov_m/Main.java
+++ src/main/java/ru/leonov_m/Main.java
@@ -5,28 +5,41 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.SuffixFileFilter;
 import org.apache.commons.io.filefilter.TrueFileFilter;
 
-import java.io.IOException;
-import java.util.*;
 import java.io.File;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws ParseException, IOException {
         Options options = new Options();
         options.addRequiredOption("p", "path", true, "Path to file/folder");
+        options.addRequiredOption("r", "regex", true, "Regex for search");
 
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = parser.parse(options, args);
 
         File file = new File(cmd.getOptionValue("p"));
-        List<String> lines = new ArrayList<>();
+        Pattern pattern = Pattern.compile(cmd.getOptionValue("r"));
+        Matcher matcher = pattern.matcher("");
 
         if (file.isFile()) {
-            lines = FileUtils.readLines(file, "UTF-8");
+            lineByRegex(FileUtils.readLines(file, "UTF-8"), matcher);
         } else if (file.isDirectory()) {
             Collection<File> files = FileUtils.listFiles(file, new SuffixFileFilter("txt"), TrueFileFilter.INSTANCE);
-            for (var t  : files) {
-                lines = FileUtils.readLines(file, "UTF-8");
+            for (var t : files) {
+                lineByRegex(FileUtils.readLines(t, "UTF-8"), matcher);
+            }
+        }
+    }
+
+    private static void lineByRegex(List<String> lines, Matcher matcher) {
+        for (var line : lines) {
+            matcher.reset(line);
+            if (matcher.find()) {
+                System.out.println(line);
             }
         }
     }
